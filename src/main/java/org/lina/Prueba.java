30c4f7669cc86c75cdb2708d5de954df41ab0a1e
francisco.resendiz <francisco.resendiz@syc.com.mx>
2014-07-10 13:28:32 -0500

    Commit clase prueba

---
+++ src/main/java/org/lina/Prueba.java
@@ -0,0 +1,11 @@
+package org.lina;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: recf
+ * Date: 10/07/14
+ * Time: 01:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Prueba {
+}
